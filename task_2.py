ca16b916f4b8bd95b72644192ea514a8500728bc
oleksandra <oleksandrabodiak@gmail.com>
2026-04-04 16:10:08 +0300

    intermediate result

--- task_2.py
+++ task_2.py
@@ -4,6 +4,22 @@ students_math_results = [
     {"name": "Іван", "scores": {"Calculus": 92, "Algebra": 88, "Discrete Math": 95}},
     {"name": "Анна", "scores": {"Calculus": 45, "Algebra": 60, "Discrete Math": 50}}
 ]
+def get_successful_students(students_list,passing_score=60):
+    successful_students = {}
+    for student in students_list:
+        name = student["name"]
+        scores = student["scores"]
+        is_successful = True
+        for score in scores.values():
+            if score < passing_score:
+                is_successful = False
+                break
+        if is_successful:
+            average_score = sum(scores.values())/len(scores.values())
+            successful_students[name] = average_score
+    return successful_students
+
+
 
 
 
