4a0eaa33c861c8db1de095e0c152f7a6ae0cafe7
oleksandra <oleksandrabodiak@gmail.com>
2026-04-04 15:34:40 +0300

    data entered

---
+++ task_1.py
@@ -0,0 +1,9 @@
+
+def evaluate_polynomial(poly_dict, x):
+    result = 0
+    for i, c_i in poly_dict.items():
+        result += c_i * x**i
+    return result
+my_poly = {0: -10, 2: 3, 4: 1}
+x1 = 2
+x2 = -1.5
