51eec942b2c794290024add58618c97eb9005787
oleksandra <oleksandrabodiak@gmail.com>
2026-04-04 15:38:44 +0300

    final result

--- task_1.py
+++ task_1.py
@@ -8,5 +8,5 @@ my_poly = {0: -10, 2: 3, 4: 1}
 x1 = 2
 x2 = -1.5
 result1 = evaluate_polynomial(my_poly, x1)
-result2 = evaluate_polynomial(my_poly, x)
-print(f'Result: {result1}, Result: {result2}')
+result2 = evaluate_polynomial(my_poly, x2)
+print(f'Result1: {result1}, Result2: {result2}')
