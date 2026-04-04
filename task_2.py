7cc8b87511cdc87c33a0418f57883b701ceeed60
oleksandra <oleksandrabodiak@gmail.com>
2026-04-04 15:47:01 +0300

    data entered

---
+++ task_2.py
@@ -0,0 +1,9 @@
+students_math_results = [
+    {"name": "Олександр", "scores": {"Calculus": 85, "Algebra": 90, "Discrete Math": 78}},
+    {"name": "Марія", "scores": {"Calculus": 65, "Algebra": 55, "Discrete Math": 70}},
+    {"name": "Іван", "scores": {"Calculus": 92, "Algebra": 88, "Discrete Math": 95}},
+    {"name": "Анна", "scores": {"Calculus": 45, "Algebra": 60, "Discrete Math": 50}}
+]
+
+
+
