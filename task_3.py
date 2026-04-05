65365b2d85f08dc7d00db0e4a624599f7c525b8d
oleksandra <oleksandrabodiak@gmail.com>
2026-04-05 11:56:53 +0300

    intermediate result

--- task_3.py
+++ task_3.py
@@ -6,4 +6,32 @@ experiments_data = [
     {"method": "Euler", "iteration": 3, "error": 0.22, "time_ms": 1.2},
     {"method": "Newton", "iteration": 1, "error": 0.05, "time_ms": 2.1}
 ]
+def analyze_methods(data):
+    report = {}
+    for entry in data:
+        method = entry["method"]
+        error = entry["error"]
+        time_ms = entry["time_ms"]
+        if method not in report:
+            report[method] = {
+            "max_error" : 0,
+            "total_time_ms" : 0,
+            "iterations_count":0 }
+        stats = report[method]
+        stats["max_error"] = max(stats["max_error"], error)
+        stats["total_time_ms"] += time_ms
+        stats["iterations_count"] += 1
+    return report
+
+
+
+
+
+
+
+
+
+
+
+
 
